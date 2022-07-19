8d3d9d831aed8fb0122c8b626903a84de85cb5a1
Arisson <arisson@pop-os.localdomain>
2022-07-19 20:25:34 -0300

    First commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
+import { Card } from '../../components/Card';
+
+import './styles.css';
+
+export function Home() {
+
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({ name: "", avatar: ""});
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    };
+    
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch("https://api.github.com/users/arissonlima");
+      const data = await response.json();
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+        <header>
+          <h1>Lista de presença</h1>
+          <div>
+            <strong>{user.name}</strong>
+            <img src={user.avatar} alt={"Foto do usuário " + user.name} />
+          </div>
+        </header>
+
+        <input
+          type="text"
+          placeholder="Digite o nome..."
+          onChange={e => setStudentName(e.target.value)}
+        />
+        <button type="button" onClick={handleAddStudent}>Adicionar</button>
+
+        {students.map(student => <Card key={student.time} name={student.name} time={student.time} />)}
+
+    </div>
+  )
+}
